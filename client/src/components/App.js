9823a5e6e6287f8defafcc07577ccde108caed14
Brian Schmalz <bschmalz@Brians-iMac.local>
2018-03-10 21:46:00 -0800

    Boilerplate setup done. Commiting to new remote repo.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,7 +7,6 @@ import Header from './Header';
 import Landing from './Landing';
 
 const Dashboard = () => <h2>Dashboard</h2>
-const SurveyNew = () => <h2>SurveyNew</h2>
 
 class App extends Component {
   
@@ -22,8 +21,7 @@ class App extends Component {
           <div>
             <Header />
             <Route exact path='/' component={Landing} />
-            <Route exact path='/surveys' component={Dashboard} />
-            <Route exact path='/surveys/new' component={SurveyNew} />
+            <Route exact path='/dashboard' component={Dashboard} />
           </div>
         </BrowserRouter>
       </div>
--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -4,9 +4,8 @@ const Landing = () => {
   return (
     <div style={{textAlign: 'center'}}>
       <h1>
-        Email!
+        Welcome Page.
       </h1>
-      Collect feedback from your users.
     </div>
   )
 }
